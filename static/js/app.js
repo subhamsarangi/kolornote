536f14dc872e81af8e7a6581259f584dbf93da1f
subhamsarangi <subhamsarangi2016@gmail.com>
2025-06-10 13:31:53 +0530

    fixed loading issue

--- static/js/app.js
+++ static/js/app.js
@@ -4,13 +4,6 @@ $(document).ready(function() {
         $('.alert').fadeOut('slow');
     }, 5000);
     
-    // Add loading state to forms
-    $('form').on('submit', function() {
-        const submitBtn = $(this).find('button[type="submit"]');
-        submitBtn.prop('disabled', true);
-        submitBtn.html('<span class="spinner-border spinner-border-sm me-2"></span>Loading...');
-    });
-    
     // Confirm navigation away from forms with data
     $('form input, form textarea, form select').on('change input', function() {
         window.onbeforeunload = function() {
